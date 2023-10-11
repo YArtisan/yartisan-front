e32b125ea19830cda8a38992b85af003016ea82b
KenTay7 <johnkta7@gmail.com>
2023-10-11 11:19:22 +0200

    Initialize styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00A6FB",
+        secondary: "#003554",
+        dark: "#051923",
+        white: "#F3F3F3",
+      },
+    },
   },
   plugins: [],
-}
+};
