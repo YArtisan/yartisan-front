542dd507c06e952765f9b13c02355603e4ff79ac
KenTay7 <johnkta7@gmail.com>
2023-10-11 11:01:29 +0200

    Initialize router & typescript

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+//@ts-ignore
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
