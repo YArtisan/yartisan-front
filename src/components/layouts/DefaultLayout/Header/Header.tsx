05193c99264457b5610e10e26754040a248ed714
MatthiasChometon <matthiaschometon787@gmail.com>
2023-12-13 12:18:35 +0100

    feat: use translations

--- src/components/layouts/DefaultLayout/Header/Header.tsx
+++ src/components/layouts/DefaultLayout/Header/Header.tsx
@@ -3,14 +3,10 @@ import React from "react";
 import { FaBars, FaBell } from "react-icons/fa";
 import { BiSolidMessage } from "react-icons/bi";
 import { BsGearFill } from "react-icons/bs";
-import { INavLink } from "@/types/interfaces";
 import NavItem from "./NavItem";
-
-const navLinks: INavLink[] = [
-  { href: "/", label: "Home" },
-  { href: "/about-us", label: "About us" },
-  { href: "/contact-us", label: "Contact us" },
-];
+import { useTranslation } from "react-i18next";
+import { INavLink } from "@/types/interfaces";
+import { useNavLinks } from "@/navigation/hooks/useNavLinks";
 
 interface IProps {
   isExpanded: boolean;
@@ -18,6 +14,7 @@ interface IProps {
 }
 
 const Header = ({ isExpanded, setIsExpanded }: IProps) => {
+  const { navLinks } = useNavLinks()
   const pathname = window.location.pathname;
   const nbMessages = 2;
   const nbNotifications = 2;
@@ -30,18 +27,16 @@ const Header = ({ isExpanded, setIsExpanded }: IProps) => {
         </a>
 
         <ul
-          className={`h-full overflow-hidden transition-[max-width,padding] duration-300 max-[910px]:left-0 max-[910px]:bg-primary-0.9 max-[910px]:w-full max-[910px]:absolute max-[910px]:top-full max-[910px]:h-[calc(100vh-80px)] ${
-            isExpanded
-              ? "max-[910px]:max-w-[450px] max-[910px]:px-5"
-              : "max-[910px]:max-w-[0px]"
-          }`}
+          className={`h-full overflow-hidden transition-[max-width,padding] duration-300 max-[910px]:left-0 max-[910px]:bg-primary-0.9 max-[910px]:w-full max-[910px]:absolute max-[910px]:top-full max-[910px]:h-[calc(100vh-80px)] ${isExpanded
+            ? "max-[910px]:max-w-[450px] max-[910px]:px-5"
+            : "max-[910px]:max-w-[0px]"
+            }`}
         >
           <div
-            className={`flex min-[910px]:items-center min-[910px]:justify-center h-full ${
-              isExpanded
-                ? "max-[910px]:flex-col max-[910px]:gap-10 max-[910px]:py-5"
-                : "max-[910px]:opacity-0"
-            }`}
+            className={`flex min-[910px]:items-center min-[910px]:justify-center h-full ${isExpanded
+              ? "max-[910px]:flex-col max-[910px]:gap-10 max-[910px]:py-5"
+              : "max-[910px]:opacity-0"
+              }`}
           >
             <Login className="min-[910px]:hidden mx-auto" />
             <div className="flex max-[910px]:flex-col max-[910px]:gap-2 min-[910px]:h-full">
@@ -65,20 +60,18 @@ const Header = ({ isExpanded, setIsExpanded }: IProps) => {
       <div className="flex items-center justify-center gap-5">
         <div className="flex gap-1">
           <div
-            className={`${
-              nbMessages > 0
-                ? "relative after:absolute after:top-0 after:right-0 after:rounded-full after:bg-red-600 after:w-2 after:h-2"
-                : ""
-            }`}
+            className={`${nbMessages > 0
+              ? "relative after:absolute after:top-0 after:right-0 after:rounded-full after:bg-red-600 after:w-2 after:h-2"
+              : ""
+              }`}
           >
             <BiSolidMessage size={25} />
           </div>
           <div
-            className={`${
-              nbNotifications > 0
-                ? "relative after:absolute after:top-0 after:right-0 after:rounded-full after:bg-red-600 after:w-2 after:h-2"
-                : ""
-            }`}
+            className={`${nbNotifications > 0
+              ? "relative after:absolute after:top-0 after:right-0 after:rounded-full after:bg-red-600 after:w-2 after:h-2"
+              : ""
+              }`}
           >
             <FaBell size={25} />
           </div>
@@ -100,15 +93,17 @@ const Login = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
+  const { t } = useTranslation()
+
   return (
     <div
       className={["flex gap-1 flex-wrap justify-center", className].join(" ")}
       {...props}
     >
       <Button template="dark" invertColors>
-        S'inscrire
+        {t('authentication:redirectToRegister')}
       </Button>
-      <Button template="dark">Se connecter</Button>
+      <Button template="dark">{t('authentication:redirectToConnect')}</Button>
     </div>
   );
 };
--- src/components/molecules/SearchBanner.tsx
+++ src/components/molecules/SearchBanner.tsx
@@ -1,11 +1,9 @@
-import Button from "@atoms/Button";
 import SearchBar from "@atoms/SearchBar";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { FaChevronDown } from "react-icons/fa";
 
-interface IProps {}
-
-function SearchBanner() {
+function SearchBanner () {
   const [query, setQuery] = useState("");
   const [filters, setFilters] = useState<IFilters>({});
 
@@ -34,28 +32,29 @@ interface IFiltersProps {
 }
 
 const Filters = ({ filters, setFilters }: IFiltersProps) => {
-  const { price, note, industry } = filters;
+  const { price } = filters;
+  const { t } = useTranslation()
 
   const handleChange = (slug: keyof IFilters, value: any) => {
     console.log("slug", value);
-    
+
     setFilters({ ...filters, [slug]: value });
   };
 
   return (
     <div className="flex gap-2 flex-wrap">
       <div className="flex items-center justify-center bg-dark rounded-md text-white font-bold px-4 py-2 gap-1">
-        <span>Industry</span>
+        <span>{t('artisanFilter:industry')}</span>
         <FaChevronDown />
       </div>
 
       <div className="flex items-center justify-center bg-dark rounded-md text-white font-bold px-4 py-2 gap-1">
-        <span>Note</span>
+        <span>{t('artisanFilter:grade')}</span>
         <FaChevronDown />
       </div>
 
       <div className="flex flex-wrap items-center justify-center bg-dark rounded-md font-bold px-4 py-2 gap-2">
-        <span className="text-white">Price</span>
+        <span className="text-white">{t('artisanFilter:price')}</span>
         <div className="flex flex-wrap gap-2 text-black">
           <input
             value={price?.min ?? 0}
