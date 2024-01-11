d8efbb38a196fde66bd3fd10b58d965de38a6c3a
MatthiasChometon <matthiaschometon787@gmail.com>
2024-01-11 19:16:35 +0100

    feat: add .env in gitignore

--- src/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import DefaultHeader from "./DefaultHeader/DefaultHeader";
+import { useDarkModeState } from "@/context/DarkModeContext";
 
 interface IProps {
   children: React.ReactNode;
