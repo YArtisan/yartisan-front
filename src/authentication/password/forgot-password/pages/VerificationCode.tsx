68410304bdd3eb9ad6fbcdb303bbac09469eb3b9
MatthiasChometon <matthiaschometon787@gmail.com>
2024-01-11 19:14:38 +0100

    feat: add forgot password

--- src/authentication/password/forgot-password/pages/VerificationCode.tsx
+++ src/authentication/password/forgot-password/pages/VerificationCode.tsx
@@ -1,13 +1,21 @@
 import { ReactElement } from "react";
-import { VerificationCodeForm } from "../components/VerificationCodeForm";
 import { useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { AuthenticationFormCard } from "@/authentication/shared/components/AuthenticationFormCard";
+import { Title } from "@/text/components/Title";
 
 export const VerificationCode = (): ReactElement => {
  const location = useLocation();
  const params = location.state ?? {};
  const { email } = params
+ const { t } = useTranslation()
 
  return (
-  <VerificationCodeForm {...{ email }} />
+  <AuthenticationFormCard {...{ cardClassName: "w-1/3" }}>
+   <Title className="mb-6">{t('authentication:forgotPasswordTitle')}</Title>
+   <div>
+    {t('authentication:resetPasswordSubTitle', { email })}
+   </div>
+  </AuthenticationFormCard>
  )
 }
